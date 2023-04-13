d06e1809f437f625ac0a5eb269707fc87716c123
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-04-13 16:57:30 +0200

    Pipes added and made them randomly moving in a loop.

--- core/src/mao/linatrix/flappybird/GameScreen.java
+++ core/src/mao/linatrix/flappybird/GameScreen.java
@@ -7,26 +7,56 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 public class GameScreen extends ScreenAdapter {
 
-    private final float BIRD_SIZE_PIXEL = 128;
+    private final float GAME_WIDTH = Gdx.graphics.getWidth();
+    private final float GAME_HEIGHT = Gdx.graphics.getHeight();
+    private final float SCALE_FACTOR = 5.F;
     private static final float MOVE_TIME = 0.1F;
     private final float EARTH_GRAVITY = 2.F;
+    private final float GAME_SPEED = 6.F;
+
+    // gap => times of bird height
+    private float gapBirdHeightFactor = 5.F;
+    private float gap;
+
     private SpriteBatch batch;
     private Texture background;
+
+    // pipes [pipeState: 0=bottom|1=top] [0=green|1=red]
+    private int numberOfPipes = 4;
+    private Texture[][] pipes;
+    private int pipeState = 0;
+    private int pipeColor = 0;
+    private float[] pipeBottomPosX = new float[numberOfPipes];
+    private float pipeBottomPosY = 0.F;
+    private float[] pipeTopPosX = new float[numberOfPipes];
+    private float pipeTopPosY = 0.F;
+    private float pipeWidth;
+    private float pipeHeight;
+    private float pipeVelocity = GAME_SPEED;
+    private float[] pipeOffset = new float[numberOfPipes];
+    private float maxPipeOffset;
+    private float distanceBetweenPipes = GAME_WIDTH * 3 / 4;
+
+    // birds [birdFlapState: 0=downflap|1=midflap|2=upflap] [birdColor: 0=redbird|1=bluebird|2=yellowbird]
     private Texture[][] birds;
     private int birdFlapState = 1;
+    private int birdColor = 0;
+    private float birdPosY = 0.F;
+    private float birdPosX = 0.F;
+    private float birdFreeFallVelocity = 0.F;
+    private float birdWidth;
+    private float birdHeight;
 
     private float timer = MOVE_TIME;
     private int invNum = 1;
 
-    private int birdColor = 2;
-
-    private float birdPosY = 0.F;
-    private float birdPosX = 0.F;
-    private float birdFreeFallVelocity = 0.F;
-
     private boolean gameState = false;
+
+    private Random randomGenerator;
 
     @Override
     public void show() {
@@ -44,8 +74,33 @@ public class GameScreen extends ScreenAdapter {
         birds[2][1] = new Texture("yellowbird-midflap.png");
         birds[2][2] = new Texture("yellowbird-upflap.png");
 
-        birdPosY = (Gdx.graphics.getHeight() - BIRD_SIZE_PIXEL) / 2.F;
-        birdPosX = (Gdx.graphics.getWidth() - BIRD_SIZE_PIXEL) / 2.F;
+        pipes = new Texture[2][2];
+        pipes[0][0] = new Texture("pipe-bottom-green.png");
+        pipes[0][1] = new Texture("pipe-bottom-red.png");
+        pipes[1][0] = new Texture("pipe-top-green.png");
+        pipes[1][1] = new Texture("pipe-top-red.png");
+
+        birdWidth = birds[birdFlapState][birdColor].getWidth() * SCALE_FACTOR;
+        birdHeight = birds[birdFlapState][birdColor].getHeight() * SCALE_FACTOR;
+
+        birdPosY = (Gdx.graphics.getHeight() - birdHeight) / 2.F;
+        birdPosX = (Gdx.graphics.getWidth() - birdWidth) / 2.F;
+
+        pipeWidth = pipes[pipeState][pipeColor].getWidth() * SCALE_FACTOR;
+        pipeHeight = pipes[pipeState][pipeColor].getHeight() * SCALE_FACTOR;
+
+        gap = birds[birdColor][birdFlapState].getHeight() * gapBirdHeightFactor * SCALE_FACTOR;
+
+        pipeBottomPosY = (((GAME_HEIGHT - gap) / 2.F) - pipeHeight);
+        pipeTopPosY = (GAME_HEIGHT + Math.abs(pipeBottomPosY) - pipeHeight);
+        maxPipeOffset = GAME_HEIGHT / 2.F - gap / 2.F -100;
+
+        randomGenerator = new Random();
+
+        for (int i = 0; i < numberOfPipes; i++) {
+            pipeOffset[i] = (randomGenerator.nextFloat() - 0.5F) * ((float) GAME_HEIGHT - gap - 200.F);
+            pipeTopPosX[i] = pipeBottomPosX[i] = (float) ((GAME_WIDTH - pipeWidth) / 2.F + i * distanceBetweenPipes);
+        }
     }
 
     @Override
@@ -69,6 +124,8 @@ public class GameScreen extends ScreenAdapter {
         if (gameState != false) {
             if (Gdx.input.isTouched()) {
                 birdFreeFallVelocity = -20;
+
+
             }
 
             if (birdPosY > 0.F  || birdFreeFallVelocity < 0.F) {
@@ -82,10 +139,22 @@ public class GameScreen extends ScreenAdapter {
             }
         }
 
-        // clearScreen();
+        clearScreen();
         batch.begin();
-        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(birds[birdColor][birdFlapState], birdPosX, birdPosY, BIRD_SIZE_PIXEL, BIRD_SIZE_PIXEL);
+        batch.draw(background, 0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+        for (int i = 0; i < numberOfPipes; i++) {
+
+            if (pipeBottomPosX[i] < -pipeWidth) {
+                pipeBottomPosX[i] = pipeTopPosX[i] += (numberOfPipes * distanceBetweenPipes);
+            } else {
+                pipeBottomPosX[i] = pipeTopPosX[i] -= pipeVelocity;
+            }
+            batch.draw(pipes[0][pipeColor], pipeBottomPosX[i], pipeBottomPosY + pipeOffset[i], pipeWidth, pipeHeight);
+            batch.draw(pipes[1][pipeColor], pipeTopPosX[i], pipeTopPosY + pipeOffset[i], pipeWidth, pipeHeight);
+        }
+
+        batch.draw(birds[birdColor][birdFlapState], birdPosX, birdPosY, birdWidth, birdHeight);
         batch.end();
 
     }
